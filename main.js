cb6068359d9723174ce05f14f09e2eb6f4875910
Max Horstmann <max.horstmann@gmail.com>
2020-04-24 21:23:24 -0400

    colors

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ function init() {
     var canvas=document.getElementById("myCanvas");
     var ctx=canvas.getContext("2d");
 
-    var sizeX = 16;
-    var sizeY = 16;
+    var sizeX = 8;
+    var sizeY = sizeX;
+
+    var fillStyleLight = "#b88b4a"
+    var fillStyleDark = "#e3c16f";
 
     var squareWidth = canvas.width / sizeX;
     var squareHeight = canvas.height / sizeY;
 
-    console.log(squareWidth);
-    console.log(squareHeight);
-
+    ctx.fillStyle = "LightGray";
 
     for(x=0;x<sizeX;x++) {
         for(y=0;y<sizeY;y++) {
-            if ((x + y) % 2 == 0) continue;
-            ctx.fillRect(x*squareWidth, y*squareHeight, squareWidth-1, squareHeight-1);
+            ctx.fillStyle = ((x + y) % 2 == 0) ? fillStyleDark : fillStyleLight;
+            ctx.fillRect(x*squareWidth, y*squareHeight, squareWidth, squareHeight);
       }
     }
 };
