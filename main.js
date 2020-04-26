bf8257f8ac01d30c7033b482fe06a030205aa255
Max Horstmann <max.horstmann@gmail.com>
2020-04-25 20:34:10 -0400

    switch to chessboardjs

--- main.js
+++ main.js
@@ -1,28 +1,18 @@
+;(function () {
 
-function init() {
-    var canvas=document.getElementById("myCanvas");
-    var ctx=canvas.getContext("2d");
-
-    var sizeX = 8;
-    var sizeY = sizeX;
-
-    var fillStyleLight = "#b88b4a"
-    var fillStyleDark = "#e3c16f";
-
-    var squareWidth = canvas.width / sizeX;
-    var squareHeight = canvas.height / sizeY;
-
-    ctx.fillStyle = "LightGray";
-
-    for(x=0;x<sizeX;x++) {
-        for(y=0;y<sizeY;y++) {
-            ctx.fillStyle = ((x + y) % 2 == 0) ? fillStyleDark : fillStyleLight;
-            ctx.fillRect(x*squareWidth, y*squareHeight, squareWidth, squareHeight);
-      }
+  function init () {
+    var config = {
+        draggable: true,
+        dropOffBoard: 'snapback', // this is the default
+        position: 'start'
     }
-};
+    var board = Chessboard('myBoard', config)
+  }
 
-init();
+  $(document).ready(init)
+
+})()
+
 
 
 
